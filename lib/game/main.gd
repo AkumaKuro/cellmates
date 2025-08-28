c4db4d9a8cd39eb14097fb4ee3a3ca1d0c0b88af
AkumaKuro <p.ferreiraherda@gmail.com>
2025-08-27 23:24:30 -0400

    Implemented engine logic into main scene

--- lib/game/main.gd
+++ lib/game/main.gd
@@ -1,60 +1,46 @@
+class_name MainGame# extends Game:
+extends Node
 
 @export var pause_screen: Control
 @export var music_player: AudioStreamPlayer
 
-class MainGame extends Game:
+@export var next_scene: PackedScene
 
-	func _init() -> void:
-
-		self._onSceneCompleteBound = self._onSceneComplete.bind(self);
-		self._setScene(TitleScene);
-
-	func _process() -> void:
-		self.updateGamePad();
-
-		if(ig.input.pressed('screenshot')):
-			window.open(document.getElementsByTagName('canvas')[0].toDataURL());
+var paused: bool = false
 
 
-		self.parent();
-		if (ig.input.pressed('pause')):
-			ig.paused = !ig.paused;
-			if(ig.paused):
-				ig.music.pause();
-			else:
-				ig.music.play();
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&'screenshot'):
+		take_screenshot()
+
+	elif event.is_action_pressed(&'pause'):
+		paused = !paused
+		pause_screen.visible = paused
+		if paused:
+			music_player.stop()
+		else:
+			music_player.play()
+
+func take_screenshot() -> void:
+	printerr('Not implemented') #TODO
 
 
 
-		if (!ig.paused):
-			this._currentScene.update();
+#
+#func draw():
+	#_currentScene.draw();
+#
+#
+#func _setScene(SceneClass):
+	#if (this._currentScene):
+		#this._currentScene.un('scene-complete', this._onSceneCompleteBound);
+#
+#
+	#var scene = SceneClass.new(this._persistenceManager, this._session);
+	#scene.on('scene-complete', this._onSceneCompleteBound);
+#
+	#self._currentScene = scene;
 
 
-
-	func draw():
-		this.parent();
-		this._currentScene.draw();
-
-		if (ig.paused):
-			pause_screen.show()
-
-
-
-	func _setScene(SceneClass):
-		if (this._currentScene):
-			this._currentScene.un('scene-complete', this._onSceneCompleteBound);
-
-
-		var scene = SceneClass.new(this._persistenceManager, this._session);
-		scene.on('scene-complete', this._onSceneCompleteBound);
-
-		this._currentScene = scene;
-
-
-	func _onSceneComplete(nextSceneClassName):
-		this._setScene(window[nextSceneClassName]);
-
-func _ready():
-	var scale = parseFloat(scale) if scale else 3;
-
-	ig.main('#canvas', MainGame, 60, 320, 240, scale);
+func _onSceneComplete() -> void:
+	get_tree().change_scene_to_packed(next_scene)
