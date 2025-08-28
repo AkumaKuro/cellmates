777f00f3934e8dfc3d7572fb3c3eec1a649872ac
AkumaKuro <p.ferreiraherda@gmail.com>
2025-08-28 00:31:00 -0400

    Implement heart

--- lib/game/entities/heart.gd
+++ lib/game/entities/heart.gd
@@ -1,135 +1,122 @@
+class_name Heart #extends Entity:
+extends CharacterBody2D
 
-class EntityHeart extends Entity:
-	var size: Vector2 = Vector2(24, 16)
-	var maxVel: Vector2 = Vector2(100, 200)
+var size: Vector2 = Vector2(24, 16)
+var maxVel: Vector2 = Vector2(100, 200)
 
+var dying: bool = false
 
-	var etype = 'heart'
-	var checkAgainst = ig.Entity.TYPE.A
+var etype = 'heart'
+var checkAgainst = 'ig.Entity.TYPE.A'
 
-	var slowPumpDuration = 2
-	var fastPumpDuration = 0.6
-	var dangerLevel = 0
-	var dangerMax = 5
+var slowPumpDuration = 2
+var fastPumpDuration = 0.6
+var dangerLevel = 0
+var dangerMax = 5
 
-	var gravityFactor = 0
-	var maxCells = 20
-	var cellCount = 20
-	var collides = ig.Entity.COLLIDES.FIXED
-	var flatline = ig.Sound.new('media/audio/flatline.*')
+var gravityFactor = 0
+var maxCells = 20
+var cellCount = 20
+var collides = 'ig.Entity.COLLIDES.FIXED'
+@export var flatline: AudioStreamPlayer
+@export var sprite: AnimatedSprite2D
 
+func _init(x, y, settings) -> void:
 
-	var animSheet = ig.AnimationSheet.new('media/heart.png', 24, 16)
+	var frameDuration = self.onePumpDuration / 2; #TODO animation speed modulation except for death anim
 
-	func _init(x, y, settings) -> void:
-		self.parent(x, y, settings);
+	self.releaseTimer = Timer.new();
 
-		var frameDuration = self.onePumpDuration / 2;
-		self.addAnim('full', frameDuration, [1,0]);
-		self.addAnim('down1', frameDuration, [3,2]);
-		self.addAnim('down2', frameDuration, [5,4]);
-		self.addAnim('down3', frameDuration, [7,6]);
-		self.addAnim('down4', frameDuration, [9,8]);
-		self.addAnim('empty', frameDuration, [11,10]);
+	self.setMode(HeartMode.Slow);
 
-		self.addAnim('heartAttack', 0.2, [12, 13], true);
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('toggle_heartrate'):
+		toggleHeartRate()
 
-		self.allAnims = [
-			self.anims.empty,
-			self.anims.down4,
-			self.anims.down3,
-			self.anims.down2,
-			self.anims.down1,
-			self.anims.full
-		];
-
-		self.releaseTimer = ig.Timer.new();
-
-		self.setMode('slow');
+func _process(delta: float) -> void:
+	if(!self.dying):
+		var mouse_position: Vector2 = get_viewport().get_mouse_position()
+		self.pos.y = clamp(mouse_position.y, 0, 25 * ig.game.collisionMap.tilesize - self.size.y);
 
 
-	func update():
-		if(!self.dying):
-			self.pos.y = ig.input.mouse.y.limit(0, 25 * ig.game.collisionMap.tilesize - self.size.y);
+		if(self.releaseTimer.delta() > 0 && self.cellCount > 0):
+			self.cellCount -= 1
+			self.releaseTimer.reset();
+			var cell = ig.game.spawnEntity(
+				EntityCell, self.center.x + self.size.x / 2,
+				self.center.y - 4, {
+				red: randf() < 0.75
+			});
 
-			if(ig.input.pressed('toggle-heartrate')):
-				self.toggleHeartRate();
-
-
-			if(self.releaseTimer.delta() > 0 && self.cellCount > 0):
-				this.cellCount -= 1
-				this.releaseTimer.reset();
-				var cell = ig.game.spawnEntity(
-					EntityCell, this.center.x + this.size.x / 2,
-					this.center.y - 4, {
-					red: Math.random() < 0.75
-				});
-
-				if(this.currentMode == 'fast'):
-					cell.vel.x *= 1.5;
+			if(self.currentMode == HeartMode.Fast):
+				cell.vel.x *= 1.5;
 
 
 
-			var index = Math.ceil(this.cellCount.map(0, this.maxCells, 0, this.allAnims.length - 1));
-			this.currentAnim = this.allAnims[index];
+		var index = ceil(self.cellCount.map(0, self.maxCells, 0, self.allAnims.length - 1));
+		self.currentAnim = self.allAnims[index];
 
-			if(this.currentMode == 'fast'):
-				this.dangerLevel += ig.system.tick;
-			else:
-				this.dangerLevel -= ig.system.tick;
-
-
-			this.dangerLevel = this.dangerLevel.limit(0, this.dangerMax);
-			if(this.dangerLevel == this.dangerMax):
-				this.die();
-
-		elif (this.currentAnim.loopCount > 0):
-			this.fireEvent('heart-death', this);
-
-
-		this.pos.x = ig.game.screen.x + 9;
-		this.parent();
-
-
-	func die():
-		if(!this.dying):
-			this.flatline.play();
-			this.dying = true;
-			this.currentAnim = this.anims.heartAttack;
-			this.currentAnim.rewind();
-
-
-
-	func toggleHeartRate():
-		if(this.currentMode == 'fast'):
-			this.setMode('slow');
+		if(self.currentMode == HeartMode.Fast):
+			self.dangerLevel += delta
 		else:
-			this.setMode('fast');
+			self.dangerLevel -= delta
+
+
+		self.dangerLevel = self.dangerLevel.limit(0, self.dangerMax);
+		if(self.dangerLevel == self.dangerMax):
+			self.die();
+
+	elif (self.currentAnim.loopCount > 0):
+		heart_death.emit()
+
+
+	self.pos.x = ig.game.screen.x + 9;
+	self.parent();
+
+signal heart_death
+
+func die():
+	if !dying:
+		flatline.play()
+		dying = true;
+		sprite.play('heart_attack')
+
+enum HeartMode {
+	Slow,
+	Fast
+}
+
+
+func toggleHeartRate():
+	if currentMode == HeartMode.Fast:
+		setMode(HeartMode.Slow);
+	else:
+		setMode(HeartMode.Fast);
 
 
 
-	func setMode(mode):
-		if(self.currentMode != mode):
-			self.currentMode = mode;
-			self.onePumpDuration = self.fastPumpDuration if mode == 'fast' else self.slowPumpDuration;
-			self.releaseTimer.set(self.onePumpDuration);
+func setMode(mode):
+	if(self.currentMode != mode):
+		self.currentMode = mode;
+		self.onePumpDuration = self.fastPumpDuration if mode == HeartMode.Fast else self.slowPumpDuration;
+		self.releaseTimer.set(self.onePumpDuration);
 
-			self.allAnims.forEach(
-				func(anim):
-					anim.frameTime = self.onePumpDuration / 2;
-			, self);
-
-
-
-	func check(other):
-		self.playerStandingOn = other.etype == 'player' && other.canStandOnCell;
-
-	var heartRate :
-		get: return Math.round(60 / this.onePumpDuration);
+		self.allAnims.forEach(
+			func(anim):
+				anim.frameTime = self.onePumpDuration / 2;
+		, self);
 
 
-	var inDanger :
-		get: return this.currentMode == 'fast';
+
+func check(other):
+	self.playerStandingOn = other.etype == 'player' && other.canStandOnCell;
+
+var heartRate :
+	get: return round(60 / onePumpDuration);
+
+
+var inDanger :
+	get: return currentMode == HeartMode.Fast;
 
 
 
